37221dc55f724ad7bfc31c9f739f37f7851b2365
Zile Cao <zman61@MacBook-Pro-63.local>
2022-09-14 03:53:34 -0400

    added footer description to project page too

--- pages/projects.js
+++ pages/projects.js
@@ -316,8 +316,8 @@ export default function Projects() {
 
           
 
-          <p class="max-w-lg mx-auto text-xs text-gray-500">
-            <span class="block mt-4"> Zile Cao | UPenn &apos;24, Econ & CS       </span>
+          <p class="max-w-xl mx-auto text-xs text-gray-500">
+            <span class="block mt-4"> &copy; 2022 Zile Cao | Special thanks to my mentor, Adam Sobotka, for his guidance on this project.       </span>
           </p>
         </div>
       </div>
